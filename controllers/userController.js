83beac7461749c7990febc1f693242149e1c910b
Erick Briones <erickalan518@gmail.com>
2022-08-18 21:25:11 -0500

    Creación de usuarios

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,33 @@
-export const createUser = async (req, res) => {
+import User from '../models/User.js';
+import bcrypt from 'bcrypt';
+import { validationResult } from 'express-validator';
 
+export const createUser = async (req, res) => {
+    // Mostrar mensajes de error de express validator
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    // Verificar si el usuario existe
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user) {
+        return res.status(400).json({ message: 'El usuario ya está registrado' });
+    }
+
+    // Crear un nuevo usuario
+    const newUser = new User(req.body);
+    const salt = await bcrypt.genSalt(10);
+    newUser.password = await bcrypt.hash(password, salt);        
+
+    try {
+        await newUser.save();
+        res.status(201).json(newUser);
+    } catch (error) {
+        console.lof(error);
+    }
+    return res.json({ message: "Usuario creado" });
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,16 @@
 import express from 'express';
 import { createUser } from '../controllers/userController.js';
+import { check } from 'express-validator';
+
 const router = express.Router();
 
-router.post('/', createUser);
+router.post('/', 
+    [
+        check('name', 'El nombre es obligatorio').not().isEmpty(),
+        check('email', 'El email no es válido').isEmail(),
+        check('password', 'La contraseña debe tener al menos 6 caracteres').isLength({ min: 6 })
+    ],
+    createUser
+);
 
 export default router;
